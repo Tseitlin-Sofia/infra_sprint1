ae6a5bcbb0a16927e8f4a9c7a478688e8fd9b752
yc-user <sofia.zeitlin@gmail.com>
2025-06-26 14:11:43 +0000

    Move infra to backend root and fix permissions

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -1,9 +1,10 @@
 import os
+from decouple import config
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
+SECRET_KEY = config('SECRET_KEY', default='django-insecure-fallback-key-for-tests')
 
 DEBUG = False
 
